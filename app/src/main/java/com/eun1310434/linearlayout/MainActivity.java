100fece88f9db570cf8716a131f5757d08a27a60
eun1310434 <eun1310434@naver.com>
2018-02-19 19:51:52 +0900

    Initial commit

---
+++ app/src/main/java/com/eun1310434/linearlayout/MainActivity.java
@@ -0,0 +1,24 @@
+/**
+ * 19.02.2018
+ * eun1310434@naver.com
+ * https://blog.naver.com/eun1310434
+ */
+
+package com.eun1310434.linearlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_horizontal);
+        //setContentView(R.layout.activity_main_vertical);
+        //setContentView(R.layout.baseline);
+        //setContentView(R.layout.gravity);
+        //setContentView(R.layout.padding);
+        //setContentView(R.layout.weight);
+    }
+}
